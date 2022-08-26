f27f6bcd17132c0c8795919de48a6f97c3da7a96
Ojingwa Daniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-08-26 07:17:53 +0100

    spread and rest

--- script.js
+++ script.js
@@ -4,3 +4,81 @@ const mutiply = (a,b)=>{
     b = typeof b  == 'undefined' ? 1 : b
     return a * b
 }
+// default parameter
+const mutiply2 = (a,b=1)=>{
+    // to make sure this does not return NaN when a user fail to input  a value we check manually if y was passed in
+    return a * b
+}
+// another example
+const greetingPlayers = ((playerName,greeting = 'Hi')=>{
+    return `${greeting} ${playerName}!`
+})
+// SPREAD
+let numbering = ((a,b,c,d)=>{
+    console.log( `a : ${a}`)
+    console.log( `b : ${b}`)
+    console.log( `c : ${c}`)
+    console.log( `d : ${d}`)
+})
+let list = ['Headphone','phone','clothes','shoe']
+// spread used to join two or more arrays
+let goalkeepers = ['Ter stergen','Pena','Tenas']
+let defenders = ['Araujo','Kounde','Christensen','Eric Garcia','Sergio Roberto','Blade']
+let strikers = ['Lewandoski','Raphinha','Ansu Fati','Dembele','Ferran Torres']
+let firstTeamPlayers = [...goalkeepers,...defenders,...strikers]
+let firstTeamPlayers2 = goalkeepers.concat(defenders,strikers,'pjanic')
+// spread in objects
+const DF = {
+    pace : true,
+    passing : 'average',
+    shooting : 'low',
+    pulling : 'high',
+    heading : 'high',
+    dribbling : 'low',
+    tall : 'high'
+}
+const DFM = {
+    space : true,
+    defensive : true,
+    passing : 'high',
+    skills : 'average',
+    tall : 'average',
+    heading : 'average',
+    tackling : 'high',
+    dribbling : 'high'
+}
+const FW = {
+    pace : true,
+    passing : true,
+    shooting : 'high',
+    heading : 'high',
+    dribbling : 'high'
+}
+const LW = {
+    pace : true,
+    passing : true,
+    shooting : 'high',
+    heading : 'Average',
+    dribbling : 'high'
+}
+const arajuo = {
+    ...DFM,
+    physically : 'high'
+}
+const deJong = {
+    ...DFM,
+    dribbling : 'high',
+    skills : "low"
+}
+const sumAll = (...nums)=>{
+     return nums.reduce((total,currentValue)=>{
+
+        return total + currentValue
+
+     })
+
+
+  }
+const details = ((firstName,lastName, ...otherName)=>{
+    console.log(`first-name : ${firstName}\n last-name : ${lastName} \n Other-name : ${otherName}`)
+})
