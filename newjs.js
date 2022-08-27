6aeb77273ebb2dfe2542119adb77837561ce3d1b
Ojingwa Daniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-08-27 12:27:17 +0100

    new object synax

--- newjs.js
+++ newjs.js
@@ -54,8 +54,58 @@ let radomValue = (list)=>{
         JerseyNumbers
     }
  }
- //computed property
-   
  
+ let result = knowYourNumber()
+ console.log(result)
+ // another example 
+ // write a function to print of the maxx,min ,avg, sum
+ let opreator = (list)=>{
+    let max = Math.max(...list)
+    let min = Math.min(...list)
+    let sum = list.reduce((total,currentValue)=> total + currentValue)
+    let avg = sum / list.length
+    return {
+        max,
+        min,
+        sum,
+        avg
+    }
+ }
+ let nums = [2,4,5,6,7,8,9]
+ let review = opreator(nums)
+ console.log(review)
+ 
+ //computed property
+ // a code to input all employers name to a object and update it 
+ const employer1 =  'benjamin'
+ let position = 'direcor'
+ 
+ const employer2 =  'emmanuel'
+ let position2 = 'Accountant'
+ 
+ let team = {
+    [position] : employer1,
+    [position2] : employer2
+ }
+ console.log(team)
+team = {
+    ...team,
+    receptionist : 'Mrs Grace'
+} 
+team = {
+    ...team,
+    Cashier : 'Mrs mercy'
+}
+let addStaff = ((obj,position,staffName)=>{
+    let listOfStaff = {
+        ...obj,
+        [position]: staffName
+    }
+    return listOfStaff
+    
+})
+let updateStaffs = addStaff(team,'lawyer','prof. Ikechukwu')
+console.log(updateStaffs)
+
 
 
