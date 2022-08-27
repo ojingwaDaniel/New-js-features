f62a1dacd4fdec8f136ef61786f74eaa49b366a9
Ojingwa Daniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-08-27 13:08:02 +0100

    adding function to object

---
+++ Object.js
@@ -0,0 +1,23 @@
+// let opreators = {
+//     add :(x,y=0)=>{
+//         return x + y
+//     },
+//     mutiply :function(x,y=1){
+//         return x * y
+//     },
+//     divde :function(x,y=1){
+//         return x / y
+//     }
+// }
+//shorthand
+let opreators = {
+   add(x,y= 0) {
+    return x + y
+   },
+   mutiply(x,y=1) {
+    return x * y
+   },
+   divide(x,y=1) {
+    return x/y
+   }
+}   
