7538487f44612232e3449615fc8c1e88a34a0564
Ojingwa Daniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-08-27 14:16:57 +0100

    method

--- Object.js
+++ Object.js
@@ -1,16 +1,16 @@
-// let opreators = {
-//     add :(x,y=0)=>{
-//         return x + y
-//     },
-//     mutiply :function(x,y=1){
-//         return x * y
-//     },
-//     divde :function(x,y=1){
-//         return x / y
-//     }
-// }
-//shorthand
 let opreators = {
+    add :(x,y=0)=>{
+        return x + y
+    },
+    mutiply :function(x,y=1){
+        return x * y
+    },
+    divde :function(x,y=1){
+        return x / y
+    }
+}
+//shorthand
+let math = {
    add(x,y= 0) {
     return x + y
    },
@@ -20,4 +20,19 @@ let opreators = {
    divide(x,y=1) {
     return x/y
    }
-}   
+} 
+// staff  regulatory time
+let timeRegulatory = [
+    {
+        staffname : 'Daniel',
+        time(hour,mins,am){
+            console.log(`today ${this.staffname} came by ${hour}:${mins} ${am} today`)
+        }
+    },
+    {
+        staffname : 'Benjamin',
+        time(hour,mins,am){
+            console.log(`today ${this.staffname} came by ${hour}:${mins} ${am} today`)
+        }
+    }
+] 
